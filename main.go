5242bb58ccd5cd4351a19d095eae653f7e435e03
mitexleo <contact@mitexleo.one>
2025-11-12 17:56:38 +0600

    Add LICENSE and initialize cobra

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import "fmt"
+import "github.com/mitexleo/backtide/cmd"
 
 func main() {
-	fmt.Println("Hello World!")
+	cmd.Execute()
 }
